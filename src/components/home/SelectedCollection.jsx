3d96789207962f4c18aca6ff6b0659c8002d83a1
Nathan Marcellous <taboodog@hotmail.com>
2025-10-26 13:59:28 -0700

    feat: add API integration with axios and loading skeleton

--- src/components/home/SelectedCollection.jsx
+++ src/components/home/SelectedCollection.jsx
@@ -1,42 +1,57 @@
-import React from "react";
-import SelectedItemVideo from "../../assets/selected-collection.mp4";
-import SelectedItemThumbnail from "../../assets/selected-collection-thumbnail.jpg";
-import SelectedItemLogo from "../../assets/selected-collection-logo.avif";
-import VerifiedIcon from "../../assets/verified.png";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import VerifiedIcon from '../../assets/verified.png';
+import Skeleton from '../ui/Skeleton';
 
 export default function SelectedCollection() {
+  const [collection, setCollection] = useState(null);
+
+  useEffect(() => {
+    async function fetchCollection() {
+      const { data } = await axios.get('https://remote-internship-api-production.up.railway.app/selectedCollection');
+
+      setCollection(data.data);
+    }
+
+    fetchCollection();
+  }, []);
+
+  if (!collection) {
+    return (
+      <header>
+        <div className='selected-collection'>
+          <Skeleton width='100%' height='100%' borderRadius='0px' />
+        </div>
+      </header>
+    );
+  }
+
   return (
     <header>
-      <div className="selected-collection">
+      <div className='selected-collection'>
         <video
           autoPlay
           muted
           loop
           playsInline
-          poster={SelectedItemThumbnail}
-          src={SelectedItemVideo}
-          className="selected-collection__bg"
+          poster={collection.thumbnail}
+          src={collection.videoLink}
+          className='selected-collection__bg'
         />
-        <div className="selected-collection__description">
-          <img
-            src={SelectedItemLogo}
-            alt=""
-            className="selected-collection__logo"
-          />
-          <h1 className="selected-collection__title">
-            Coachella Throwback Merchandise Trunk
-          </h1>
-          <Link to={'/user'} className="selected-collection__author">
-            By Coachella
-            <img
-              src={VerifiedIcon}
-              className="selected-collection__author__verified"
-            />
+        <div className='selected-collection__description'>
+          <img src={collection.logo} alt='' className='selected-collection__logo' />
+          <h1 className='selected-collection__title'>{collection.title}</h1>
+          <Link to={`/user/${collection.creatorId}`} className='selected-collection__author'>
+            By {collection.creator}
+            <img src={VerifiedIcon} className='selected-collection__author__verified' />
           </Link>
-          <div className="selected-collection__details">10 items · 3.4 ETH</div>
-          <Link to={'/collection'} className="selected-collection__button">
-            <div className="green-pulse"></div>
+          <div className='selected-collection__details'>
+            {collection.amountOfItems} items · {collection.floorPrice} ETH
+          </div>
+          <Link to={`/collection/${collection.collectionId}`} className='selected-collection__button'>
+            <div className='green-pulse'></div>
             View Collection
           </Link>
         </div>
