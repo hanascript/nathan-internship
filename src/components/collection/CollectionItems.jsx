b6d6c3a323f2e218d37eb90e7440649d994d5986
Nathan Marcellous <taboodog@hotmail.com>
2025-10-28 10:24:57 -0700

    feat: collection header dynamic data

--- src/components/collection/CollectionItems.jsx
+++ src/components/collection/CollectionItems.jsx
@@ -1,52 +1,48 @@
-import { faEye } from "@fortawesome/free-regular-svg-icons";
-import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { Link } from "react-router-dom";
+import { faEye } from '@fortawesome/free-regular-svg-icons';
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function CollectionItems() {
   return (
-    <section id="collection-items">
-      <div className="row collection-items__row">
-        <div className="collection-items__header">
-          <div className="collection-items__header__left">
-            <span className="collection-items__header__live">
-              <div className="green-pulse"></div>
+    <section id='collection-items'>
+      <div className='row collection-items__row'>
+        <div className='collection-items__header'>
+          <div className='collection-items__header__left'>
+            <span className='collection-items__header__live'>
+              <div className='green-pulse'></div>
               Live
             </span>
-            <span className="collection-items__header__results">
-              10 results
-            </span>
+            <span className='collection-items__header__results'>10 results</span>
           </div>
-          <select className="collection-items__header__sort">
-            <option value="" default>
+          <select className='collection-items__header__sort'>
+            <option value='' default>
               Default
             </option>
-            <option value="">Price high to low</option>
-            <option value="">Price low to high</option>
+            <option value=''>Price high to low</option>
+            <option value=''>Price low to high</option>
           </select>
         </div>
-        <div className="collection-items__body">
+        <div className='collection-items__body'>
           {new Array(8).fill(0).map((_, index) => (
-            <div className="item-column">
-              <Link to={"/item"} key={index} className="item">
-                <figure className="item__img__wrapper">
+            <div className='item-column' key={index}>
+              <Link to={'/item'} className='item'>
+                <figure className='item__img__wrapper'>
                   <img
-                    src="https://i.seadn.io/gcs/files/0a085499e0f3800321618af356c5d36b.png?auto=format&dpr=1&w=384"
-                    alt=""
-                    className="item__img"
+                    src='https://i.seadn.io/gcs/files/0a085499e0f3800321618af356c5d36b.png?auto=format&dpr=1&w=384'
+                    alt=''
+                    className='item__img'
                   />
                 </figure>
-                <div className="item__details">
-                  <span className="item__details__name">Meebit #0001</span>
-                  <span className="item__details__price">0.98 ETH</span>
-                  <span className="item__details__last-sale">
-                    Last sale: 7.45 ETH
-                  </span>
+                <div className='item__details'>
+                  <span className='item__details__name'>Meebit #0001</span>
+                  <span className='item__details__price'>0.98 ETH</span>
+                  <span className='item__details__last-sale'>Last sale: 7.45 ETH</span>
                 </div>
-                <div className="item__see-more">
-                  <button className="item__see-more__button">See More</button>
-                  <div className="item__see-more__icon">
+                <div className='item__see-more'>
+                  <button className='item__see-more__button'>See More</button>
+                  <div className='item__see-more__icon'>
                     <FontAwesomeIcon icon={faShoppingBag} />
                   </div>
                 </div>
@@ -55,7 +51,7 @@ export default function CollectionItems() {
           ))}
         </div>
       </div>
-      <button className="collection-page__button">Load more</button>
+      <button className='collection-page__button'>Load more</button>
     </section>
   );
 }
--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,16 +1,34 @@
-import React, { useEffect } from "react";
-import CollectionHeader from "../components/collection/CollectionHeader";
-import CollectionInfo from "../components/collection/CollectionInfo";
-import CollectionItems from "../components/collection/CollectionItems";
+import React, { useEffect } from 'react';
+import CollectionHeader from '../components/collection/CollectionHeader';
+import CollectionInfo from '../components/collection/CollectionInfo';
+import CollectionItems from '../components/collection/CollectionItems';
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../components/hooks/useFetch';
 
 export default function CollectionPage() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const { id } = useParams();
+
+  const { data: collection, loading, error } = useFetch(`/collection/${id}`);
+
   return (
     <>
-      <CollectionHeader />
+      <CollectionHeader
+        title={collection.title}
+        logo={collection.logo}
+        imageLink={collection.imageLink}
+        creatorId={collection.creatorId}
+        creator={collection.creator}
+        totalVolume={collection.totalVolume}
+        floor={collection.floor}
+        bestOffer={collection.bestOffer}
+        listed={collection.listed}
+        owners={collection.owners}
+        loading={loading}
+      />
       <CollectionInfo />
       <CollectionItems />
     </>
