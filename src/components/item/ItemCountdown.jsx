366c0af4ad26ae7ca5ba57623d500d4929ec998b
Nathan Marcellous <taboodog@hotmail.com>
2025-10-28 16:36:04 -0700

    fix: countdown calculation readability

--- src/components/item/ItemCountdown.jsx
+++ src/components/item/ItemCountdown.jsx
@@ -20,9 +20,10 @@ export default function ItemCountdown({ expiryDate }) {
   }, [expiryDate]);
 
   const formatCountdown = countdown => {
-    const hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
+    const totalSeconds = Math.floor(countdown / 1000)
+    const seconds = totalSeconds % 60;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const hours = Math.floor(totalSeconds / (60 * 60)) % 24;
 
     if (hours === 0 && minutes === 0 && seconds === 0) {
       return 'Expired';
